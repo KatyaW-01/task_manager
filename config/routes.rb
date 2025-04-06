e66dcd1bdc9be34917dddcda2c7c9c15831982e1
Katya Weicht <katya.weicht@gmail.com>
2025-04-05 18:12:48 -0600

    Add ability to make delete requests

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   get "/api/v1/tasks/:id", to: "api/v1/tasks#show"
   post "/api/v1/tasks", to: "api/v1/tasks#create"
   patch '/api/v1/tasks/:id', to: "api/v1/tasks#update"
+  delete '/api/v1/tasks/:id', to: "api/v1/tasks#destroy"
 end
