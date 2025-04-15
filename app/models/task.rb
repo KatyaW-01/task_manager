61c2805fda88268987725b2c5b40c31813a9574d
Katya Weicht <katya.weicht@gmail.com>
2025-04-15 15:35:34 -0600

    Start model testing

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ApplicationRecord
+  def laundry?
+    if title == "laundry"
+      return true
+    elsif description == "laundry"
+      return true
+    else
+      return false
+    end
+  end
 
 end
