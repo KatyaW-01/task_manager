9c207cecc95e50d619df987073b24a0b9937b176
Katya Weicht <katya.weicht@gmail.com>
2025-05-18 14:22:10 -0600

    Add cors gem and connect to react application

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -8,10 +8,21 @@ RSpec.describe Task, type: :model do
 
         expect(task.laundry?).to be(true)
       end
-      
+
       it "returns true when the description is laundry" do
         task = Task.create!(title: "Clean my clothes", description: "laundry")
     
+        expect(task.laundry?).to be(true)
+      end
+      it "returns false when neither the description nor title is laundry" do
+      task = Task.create!(title: "Wash the dishes", description: "dishes")
+
+      expect(task.laundry?).to be(false)
+      end
+
+      it "is case insensitive when checking if the title contains the word laundry" do
+        task = Task.create!(title: "Laundry", description: "clean clothes")
+
         expect(task.laundry?).to be(true)
       end
 
